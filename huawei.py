0437cc6853e9be54e3b65ad4c8eb64e6a6cc90a6
amadeuszg22 <amadeusz.grabowski@gmail.com>
2019-08-27 18:10:07 +0200

    Total data statistics added: Total conection time

--- huawei.py
+++ huawei.py
@@ -24,6 +24,7 @@ class config:
 	cur_up_speed = 0
 	tot_up_data = 0
 	tot_down_data = 0
+	tot_con_time = 0
 
 def ping(host):
 	probe = pyping.ping(host)
@@ -82,7 +83,8 @@ class pool:
 		config.cur_up_speed = ((float(sup.response.currentuploadrate.get_text(strip=True))/1024)/1024)
                 config.tot_up_data = (((float(sup.response.totalupload.get_text(strip=True))/1024)/1024)/1024)
 		config.tot_down_data = (((float(sup.response.totaldownload.get_text(strip=True))/1024)/1024)/1024)
-		if config.cur_con_time and config.cur_up and config.cur_down and config.cur_down_speed and config.cur_up_speed and config.tot_up_data and config.tot_down_data  > 0:
+		config.tot_con_time = (float(sup.response.totalconnecttime.get_text(strip=True))/60)/60
+		if config.cur_con_time and config.cur_up and config.cur_down and config.cur_down_speed and config.cur_up_speed and config.tot_up_data and config.tot_down_data and config.tot_con_time  > 0:
                         return True
                 else:
                         return False
@@ -105,6 +107,7 @@ while True:
 					print (time.ctime(),"Unable to get data from modem")
 				if pool.traffic_stat() is not False:
 					print (time.ctime(),'Current connection time:{:6.2f}'.format(config.cur_con_time))
+					print (time.ctime(),'Total connection time:{:6.2f}'.format(config.tot_con_time))
 					print (time.ctime(),'Transmited data:{:6.2f}GB'.format(config.cur_up))
 					print (time.ctime(),'Received data:{:6.2f}GB'.format(config.cur_down))
 					print (time.ctime(),'Current Download speed:{:6.2f}Mbps'.format(config.cur_down_speed))
@@ -112,7 +115,7 @@ while True:
 					print (time.ctime(),'Total Upload data:{:6.2f}GB'.format(config.tot_up_data))
 					print (time.ctime(),'Total Download data:{:6.2f}GB'.format(config.tot_down_data))
 				else:
-					print (time.ctime(),"Unable to get data from modem")
+					print (time.ctime(),"Unable to get statisctics from modem")
 			else:
 				print (time.ctime(),"Unauthorised")
 		else:
